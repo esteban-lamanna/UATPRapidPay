df7ac5dd1665136ad3054c86266c0d4a4d6618b3
Esteban Lamanna <esteban.lamanna@distillery.com>
2021-10-28 17:18:15 -0300

    basic auth works fine

---
+++ RapidPay/Authentication/BasicAuthenticationHandler.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RapidPay.Logic;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RapidPay.Authentication
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<BasicAuthenticationOptions>
+    {
+        readonly IUserLoginLogic _userLoginLogic;
+
+        public BasicAuthenticationHandler(IOptionsMonitor<BasicAuthenticationOptions> options,
+                                          ILoggerFactory logger,
+                                          UrlEncoder encoder,
+                                          ISystemClock clock,
+                                          IUserLoginLogic userLoginLogic)
+        : base(options, logger, encoder, clock)
+        {
+            _userLoginLogic = userLoginLogic;
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            Response.Headers.Add("WWW-Authenticate", "Basic");
+
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return Task.FromResult(AuthenticateResult.Fail("Authorization header missing."));
+
+            // Get authorization key
+            var authorizationHeader = Request.Headers["Authorization"].ToString();
+            var authHeaderRegex = new Regex(@"Basic (.*)");
+
+            if (!authHeaderRegex.IsMatch(authorizationHeader))
+                return Task.FromResult(AuthenticateResult.Fail("Authorization code not formatted properly."));
+
+            var authBase64 = Encoding.UTF8.GetString(Convert.FromBase64String(authHeaderRegex.Replace(authorizationHeader, "$1")));
+            var authSplit = authBase64.Split(Convert.ToChar(":"), 2);
+            var authUsername = authSplit[0];
+            var password = authSplit[1];
+
+            var validUser = _userLoginLogic.Authenticate(authUsername, password).GetAwaiter().GetResult();
+
+            if (!validUser)
+                return Task.FromResult(AuthenticateResult.Fail("Invalid credentials"));
+
+            var list = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, authUsername),
+            };
+
+            var identity = new ClaimsIdentity(list, "Basic");
+
+            var claimsPrincipal = new ClaimsPrincipal(identity);
+
+            return Task.FromResult(AuthenticateResult.Success(new AuthenticationTicket(claimsPrincipal, Scheme.Name)));
+        }
+    }
+}
---
+++ RapidPay/Logic/UserLoginLogic.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RapidPay.Logic.Entities;
+using RapidPay.Repository;
+using System.Threading.Tasks;
+
+namespace RapidPay.Logic
+{
+    public class UserLoginLogic : IUserLoginLogic
+    {
+        readonly RapidPayContext _rapidPayContext;
+
+        public UserLoginLogic(RapidPayContext rapidPayContext)
+        {
+            _rapidPayContext = rapidPayContext;
+        }
+
+        public async Task<bool> Authenticate(string authUsername, string password)
+        {
+            return await _rapidPayContext.Set<User>()
+                                   .AnyAsync(a => a.Email == authUsername && a.Password == password);
+        }
+    }
+}
